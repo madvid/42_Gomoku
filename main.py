c66e2219729bafd6daa689a366b718ce564639f8
Matthieu DAVID <mdavid@e1r11p17.42.fr>
2021-11-09 20:27:11 +0100

    WIP: UI

--- main.py
+++ main.py
@@ -28,134 +28,123 @@ dct_stylesheet = {"menu_button": "*{border: 4px solid '#1B5DBF';" +
 								  "*:hover{background: '#0067FF';}"}
 
 
-class MyWindow(QMainWindow):
+class MyWindow(QWidget):
 	def __init__(self):
 		super(MyWindow, self).__init__()
 		self.setWindowTitle("Gomoku by mdavid & ppeigne")
 		self.setGeometry(200, 200, 1080, 720)
-		print("height", self.geometry().height())
-		print("width", self.geometry().width())
 		self.setStyleSheet("background: #152338;")
+		self.grid = QGridLayout()
 		self.initUI()
+		self.menu_frame()
+		#self.select_frame()
+		self.setLayout(self.grid)
+
 
 	def initUI(self):
-		# buttons and label
-		self.button_pvp = QPushButton("Player vs Player", self)
-		self.button_pva1 = QPushButton("Player vs Algo 1", self)
-		self.button_pva2 = QPushButton("Player vs Algo 2", self)
+		# -------- MAIN MENU FRAME -------- #
+		# Display logo
+		image_main = QPixmap("assets/Gomoku.png")
+		self.logo_main = QLabel()
+		self.logo_main.setPixmap(image_main)
+		self.logo_main.setAlignment(QtCore.Qt.AlignCenter)
+		self.logo_main.setStyleSheet("margin-top: 0px;")
+		
+		# Button and Label widgets
+		self.button_pvp = QPushButton("Player vs Player")
+		self.button_pva = QPushButton("Player vs Algo 1")
 
 		self.button_pvp.setGeometry(360, 160, 360, 80)
-		self.button_pva1.setGeometry(360, 320, 360, 80)
-		self.button_pva2.setGeometry(360, 480, 360, 80)
+		self.button_pva.setGeometry(360, 320, 360, 80)
 
+		self.button_pvp.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_pva.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_pvp.setStyleSheet(dct_stylesheet["menu_button"])
+		self.button_pva.setStyleSheet(dct_stylesheet["menu_button"])
+
+		#self.button_pvp.clicked.connect(self.game_pvp)
+		#self.button_pva.clicked.connect(self.game_pva)
+
+		# -------- SELECT PERSO FRAME -------- #
 		# Display logo
-		image = QPixmap("assets/Gomoku.png")
-		logo = QLabel()
-		logo.setPixmap(image)
-		logo.setAlignment(QtCore.Qt.AlignCenter)
-		logo.setStyleSheet("margin-top: 0px;")
+		image_select = QPixmap("assets/character_selection.png")
+		self.logo_select = QLabel()
+		self.logo_select.setPixmap(image_select)
+		self.logo_select.setAlignment(QtCore.Qt.AlignCenter)
+		self.logo_select.setStyleSheet("margin-top: 0px;")
 		
-		# Button widget:
-		button_pvp = QPushButton("PLAY")
-		button_pvp.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_pvp.setStyleSheet(dct_stylesheet["menu_button"])
-		grid.addWidget(logo, 0, 0)
-		grid.addWidget(button_pvp, 1, 0)
+		# Buttons widget:
+		self.button_player1 = QPushButton("Perso 1")
+		self.button_player2 = QPushButton("Perso 2")
+		self.button_player3 = QPushButton("Perso 3")
+		self.button_player4 = QPushButton("Perso 4")
+		self.button_player5 = QPushButton("Perso 5")
+		self.button_player6 = QPushButton("Perso 6")
+		self.button_play = QPushButton("PLAY")
+		self.button_back = QPushButton("BACK")
+		
+		self.button_player1.setFixedWidth(200)
+		self.button_player1.setFixedHeight(200)
+		self.button_player2.setFixedWidth(200)
+		self.button_player2.setFixedHeight(200)
+		self.button_player3.setFixedWidth(200)
+		self.button_player3.setFixedHeight(200)
+		self.button_player4.setFixedWidth(200)
+		self.button_player4.setFixedHeight(200)
+		self.button_player5.setFixedWidth(200)
+		self.button_player5.setFixedHeight(200)
+		self.button_player6.setFixedWidth(200)
+		self.button_player6.setFixedHeight(200)
 
-		self.button_pvp.clicked.connect(self.game_pvp)
-		self.button_pva1.clicked.connect(self.game_pva1)
-		self.button_pva2.clicked.connect(self.game_pva2)
+		self.button_player1.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_player2.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_player3.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_player4.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_player5.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_player6.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_play.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+		self.button_back.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+
+		self.button_player1.setStyleSheet(dct_stylesheet["character"])
+		self.button_player2.setStyleSheet(dct_stylesheet["character"])
+		self.button_player3.setStyleSheet(dct_stylesheet["character"])
+		self.button_player4.setStyleSheet(dct_stylesheet["character"])
+		self.button_player5.setStyleSheet(dct_stylesheet["character"])
+		self.button_player6.setStyleSheet(dct_stylesheet["character"])
+		self.button_play.setStyleSheet(dct_stylesheet["choose_frame"])
+		self.button_back.setStyleSheet(dct_stylesheet["choose_frame"])
 	
 	def menu_frame(self):
-		self.initUI()
+		#self.initUI()
+		# Placing all the widgets in the main menu frame:
+		self.grid.addWidget(self.logo_main, 0, 0)
+		self.grid.addWidget(self.button_pvp, 1, 0)
+		self.grid.addWidget(self.button_pva, 2, 0)
 	
+	def select_frame(self):
+		# Placing all the widgets in the select character frame:
+		self.grid.addWidget(self.logo_select, 0, 0, 1, 3)
+		self.grid.addWidget(self.button_player1, 2, 0)
+		self.grid.addWidget(self.button_player2, 2, 1)
+		self.grid.addWidget(self.button_player3, 2, 2)
+		self.grid.addWidget(self.button_player4, 3, 0)
+		self.grid.addWidget(self.button_player5, 3, 1)
+		self.grid.addWidget(self.button_player6, 3, 2)
+		
+		self.grid.addWidget(self.button_play, 4, 0)
+		self.grid.addWidget(self.button_back, 4, 2)
 	
-		def game_pvp():
-			pass
+	def game_pvp():
+		pass
 
-		def game_pva1():
-			pass
-
-		def game_pva2():
-			pass
+	def game_pva():
+		pass
 		
 
 def window():
 	app = QApplication(sys.argv)
-	#win = MyWindow()
-	win = QWidget()
-	win.setWindowTitle("Gomoku by ppeigne and mdavid")
-	#win.setFixedWidth(1000)
-	win.setGeometry(200, 200, 1080, 720)
+	win = MyWindow()
 
-	grid = QGridLayout()
-
-	def menu_frame():
-
-	def select_perso_frame():
-		# Display logo
-		image = QPixmap("assets/character_selection.png")
-		logo = QLabel()
-		logo.setPixmap(image)
-		logo.setAlignment(QtCore.Qt.AlignCenter)
-		logo.setStyleSheet("margin-top: 0px;")
-		# Buttons widget:
-		button_player1 = QPushButton("Perso 1")
-		button_player2 = QPushButton("Perso 2")
-		button_player3 = QPushButton("Perso 3")
-		button_player4 = QPushButton("Perso 4")
-		button_player5 = QPushButton("Perso 5")
-		button_player6 = QPushButton("Perso 6")
-		button_play = QPushButton("PLAY")
-		button_back = QPushButton("BACK")
-		
-		button_player1.setFixedWidth(200)
-		button_player1.setFixedHeight(200)
-		button_player2.setFixedWidth(200)
-		button_player2.setFixedHeight(200)
-		button_player3.setFixedWidth(200)
-		button_player3.setFixedHeight(200)
-		button_player4.setFixedWidth(200)
-		button_player4.setFixedHeight(200)
-		button_player5.setFixedWidth(200)
-		button_player5.setFixedHeight(200)
-		button_player6.setFixedWidth(200)
-		button_player6.setFixedHeight(200)
-
-		button_player1.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_player2.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_player3.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_player4.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_player5.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_player6.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_play.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-		button_back.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-
-		button_player1.setStyleSheet(dct_stylesheet["character"])
-		button_player2.setStyleSheet(dct_stylesheet["character"])
-		button_player3.setStyleSheet(dct_stylesheet["character"])
-		button_player4.setStyleSheet(dct_stylesheet["character"])
-		button_player5.setStyleSheet(dct_stylesheet["character"])
-		button_player6.setStyleSheet(dct_stylesheet["character"])
-		button_play.setStyleSheet(dct_stylesheet["choose_frame"])
-		button_back.setStyleSheet(dct_stylesheet["choose_frame"])
-		
-		grid.addWidget(logo, 0, 0, 1, 3)
-		
-		grid.addWidget(button_player1, 2, 0)
-		grid.addWidget(button_player2, 2, 1)
-		grid.addWidget(button_player3, 2, 2)
-		grid.addWidget(button_player4, 3, 0)
-		grid.addWidget(button_player5, 3, 1)
-		grid.addWidget(button_player6, 3, 2)
-		
-		grid.addWidget(button_play, 4, 0)
-		grid.addWidget(button_back, 4, 2)
-
-	#menu_frame()
-	select_perso_frame()
-
-	win.setLayout(grid)
 	win.show()
 	sys.exit(app.exec())
 
