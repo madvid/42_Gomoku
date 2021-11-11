910479ca1d9f439e393d07e41620078c15860c19
ppeigne <ppeigne@student.42.fr>
2021-11-11 10:56:35 +0100

    start class Node

---
+++ game/board.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numpy import ndarray
+from typing import Tuple, List, NamedTuple
+
+# Position = NamedTuple[int,int]
+
+class Node():
+    # Global attributes of all nodes
+    grid_shape = None
+    
+    def __init__(self, grid: ndarray, color: int = 1) -> None:
+        self.grid = grid
+        self.color = color
+    
+    def update(self, pos: Tuple[int,int], color: int = 1) -> Node:
+        tmp_grid = np.deepcopy(self.grid)
+        tmp_grid[pos] = color
+        return Node(tmp_grid)
+
+    def generate_next_moves(self) -> List[Node]:
+        possibles_moves = np.argwhere(self.grid == 0)
+        return [self.update(pos, self.color * -1) for pos in possibles_moves]
