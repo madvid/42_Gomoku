7676191ee1c62469a265282c49c57eb2307aa9e7
ppeigne <ppeigne@student.42.fr>
2021-11-12 21:20:01 +0100

    fix: rename diag test starting at 1

--- game/tests/measure_diag_test.py
+++ game/tests/measure_diag_test.py
@@ -1,31 +1,6 @@
 from metrics import *
 
-# def test_diag1():
-#     g = np.array([
-#         [1, 0, 0],
-#         [0, 1, 0],
-#         [0, 1, 0]
-#         ])
-#     assert measure_diag(g, 1) == [Diagonal(2, (0, 0), True)]
-
-
-# def test_diag2():
-#     g = np.array([
-#         [1, 0, 0],
-#         [0, 1, 0],
-#         [0, 1, 1]
-#         ])
-#     assert measure_diag(g, 1) == [Diagonal(3, (0, 0), True)]
-
-# def test_diag3():
-#     g = np.array([
-#         [0, 0, 0],
-#         [0, 1, 0],
-#         [0, 1, 1]
-#         ])
-#     assert measure_diag(g, 1) == [Diagonal(2, (1, 1), True)]
-
-def test_diag4():
+def test_diag1():
     g = np.array([
         [0, 0, 0, 0],
         [0, 1, 0, 0],
@@ -34,7 +9,7 @@ def test_diag4():
         ])
     assert measure_diag(g, 1) == [Diagonal(2, (1, 1), True)]
 
-def test_diag5():
+def test_diag2():
     g = np.array([
         [0, 0, 0, 0],
         [1, 0, 0, 0],
@@ -43,7 +18,7 @@ def test_diag5():
         ])
     assert measure_diag(g, 1) == [Diagonal(3, (1, 0), True)]
 
-def test_diag6():
+def test_diag3():
     g = np.array([
         [0, 1, 0, 0],
         [0, 0, 1, 0],
@@ -52,7 +27,7 @@ def test_diag6():
         ])
     assert measure_diag(g, 1) == [Diagonal(2, (2, 0), True), Diagonal(3, (0,1), True)]
 
-def test_diag7():
+def test_diag4():
     g = np.array([
         [0, 1, 0, 1],
         [0, 0, 1, 0],
@@ -64,7 +39,7 @@ def test_diag7():
                                 Diagonal(3, (0,1), True),
                                 Diagonal(3, (0,3), False)]
 
-def test_diag8():
+def test_diag5():
     g = np.array([
         [0, 0, 0, 0],
         [1, 0, 1, 0],
@@ -73,44 +48,3 @@ def test_diag8():
         ])
     assert measure_diag(g, 1) == [Diagonal(2, (1, 0), True), Diagonal(2, (1, 2), True),
                                   Diagonal(3, (1, 2), False)]
-
-# def test_diag1():
-#     g = np.array([
-#         [1, 0, 0],
-#         [1, 0, 0],
-#         [1, 1, 0]
-#         ])
-#     assert measure_diag(g, 1) == [Diagonal(2, (1, 0))]
-
-# def test_diag2():
-#     g = np.array([
-#         [1, 0, 0],
-#         [1, 3, 0],
-#         [1, 1, 1]
-#         ])
-#     assert measure_diag(g, 1) == 3
-
-# def test_diag3():
-#     g = np.array([
-#         [1, 1, 0],
-#         [1, 0, 1],
-#         [1, 0, 1]
-#         ])
-#     assert measure_diag(g, 1) == 2
-
-
-# def test_diag4():
-#     g = np.array([
-#         [0, 1, 1],
-#         [1, 1, 0],
-#         [1, 0, 1]
-#         ])
-#     assert measure_diag(g, 1) == 2
-
-# def test_diag5():
-#     g = np.array([
-#         [1, 0, 0],
-#         [1, 0, 0],
-#         [0, 0, 1]
-#         ])
-#     assert measure_diag(g, 1) == 1
