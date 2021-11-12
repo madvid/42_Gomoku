155bbc93ec1b50013964c24714272755389595f4
ppeigne <ppeigne@student.42.fr>
2021-11-12 09:32:31 +0100

    update: change measures_row -> return a List[Row] instead of an int

--- game/metrics.py
+++ game/metrics.py
@@ -1,21 +1,42 @@
 from __future__ import annotations
+from typing import List, Tuple, NamedTuple
 import numpy as np
 from numpy import ndarray
 
 BLACK = 1
 WHITE = -1
 
-def measure_row(grid: ndarray, color: int) -> int:
-    max_len = 0
-    for r in grid:
+Position = Tuple[int, int] # The indexing of positions follows numpy's one (height,width)
+
+Row = NamedTuple('Row', [('lenght', int), ('position', Position)])
+Column = NamedTuple('Column', [('lenght', int), ('position', Position)])
+Diagonal = NamedTuple('Diagonal', [('lenght', int), ('position', Position)])
+
+# Row = Tuple[int, Position]
+# Column = Tuple[int, Position]
+# Diagonal = Tuple[int, Position]
+
+def measure_row(grid: ndarray, color: int) -> List[Row]:
+    rows = []
+    height,width = grid.shape
+    for i, r in enumerate(grid):
         len_ = 0
-        for i in r:
-            if i == color:
+        start_i, start_j = i, 0
+        for j, x in enumerate(r):
+            if x == color:
                 len_ += 1
-                max_len = len_ if len_ > max_len else max_len
+                if len_ > 1 and j == width-1:
+                    rows.append(Row(len_, (start_i, start_j)))
+                    len_ = 0
             else:
-                len_ = 0
-    return max_len    
+                if len_ < 2:
+                    start_j += 1
+                    len_ = 0
+                else:
+                    rows.append(Row(len_, (start_i, start_j)))
+                    len_ = 0
+                    start_i, start_j = i, j+1
+    return rows
 
 def measure_col(grid: ndarray, color: int) -> int:
     return measure_row(grid.T, color)
@@ -33,6 +54,34 @@ def measure_diag(grid: ndarray, color: int) -> int:
                     len_ = 0
     return max_len    
 
+
+# def measure_row(grid: ndarray, color: int) -> int:
+#     max_len = 0
+#     for r in grid:
+#         len_ = 0
+#         for i in r:
+#             if i == color:
+#                 len_ += 1
+#                 max_len = len_ if len_ > max_len else max_len
+#             else:
+#                 len_ = 0
+#     return max_len    
+
+# def measure_col(grid: ndarray, color: int) -> int:
+#     return measure_row(grid.T, color)
+
+# def measure_diag(grid: ndarray, color: int) -> int:
+#     max_len = 0
+#     for d in range(len(grid)):
+#         len_ = 0
+#         for i in range(d, len(grid)):
+#             for j in range(d, len(grid)):
+#                 if grid[d+i, d+j] == color:
+#                     len_ += 1
+#                     max_len = len_ if len_ > max_len else max_len
+#                 else:
+#                     len_ = 0
+#     return max_len    
 
 def stone_sum(grid: ndarray, color: int) -> int:
     # Returns the difference between the total of black and white stones. The bigger the better.
--- game/tests/measure_row_test.py
+++ game/tests/measure_row_test.py
@@ -6,7 +6,8 @@ def test_row1():
         [1, 0, 0],
         [1, 1, 0]
         ])
-    assert measure_row(g, 1) == 2
+    print(measure_row(g, 1))
+    assert measure_row(g, 1) == [Row(2, (2,0))]
 
 def test_row2():
     g = np.array([
@@ -14,29 +15,41 @@ def test_row2():
         [1, 0, 0],
         [1, 1, 1]
         ])
-    assert measure_row(g, 1) == 3
+    print(measure_row(g, 1))
+    assert measure_row(g, 1) == [Row(3, (2,0))]
+
+test_row1()
+test_row2()
 
 def test_row3():
     g = np.array([
         [1, 0, 0],
         [1, 0, 0],
-        [1, 0, 1]
+        [0, 1, 1]
         ])
-    assert measure_row(g, 1) == 1
+    assert measure_row(g, 1) == [Row(2, (2,1))]
 
 
 def test_row4():
     g = np.array([
         [0, 1, 1],
-        [1, 0, 0],
-        [1, 0, 0]
+        [1, 1, 0],
+        [1, 1, 1]
         ])
-    assert measure_row(g, 1) == 2
+    assert measure_row(g, 1) == [Row(2, (0, 1)), Row(2, (1, 0)), Row(3, (2,0))]
 
 def test_row5():
     g = np.array([
-        [1, 0, 0],
-        [1, 0, 0],
+        [0, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
+        ])
+    assert measure_row(g, 1) == []
+
+def test_row6():
+    g = np.array([
+        [0, 0, 1],
+        [1, 0, 1],
         [0, 0, 1]
         ])
-    assert measure_row(g, 1) == 1
+    assert measure_row(g, 1) == []
