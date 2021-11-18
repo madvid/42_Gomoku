f39375603928905ae37fee1d509952f02321a851
Md4 <matthieudavid404@gmail.com>
2021-11-18 15:25:37 +0100

    [game process] initiation of game sequence, the agent

--- game.py
+++ game.py
@@ -4,10 +4,30 @@
 from interface.game_interface import mywindow
 from game.minimax import solver
 import numpy as np
+from __future__ import annotations
+
+# =========================================================================== #
+#                          | constants definition |                           #
+# =========================================================================== #
 
 
+# =========================================================================== #
+#                          | fonctions definition |                           #
+# =========================================================================== #
+
+def initiate_instance(g:Game):
+	parent = Node(None, g, -1)
+	g.parent.nb_free_three = 0
+	color = 1
+	node = Node(parent, g, color)
+	
+
+
+# =========================================================================== #
+#                           | Classes definition |                            #
+# =========================================================================== #
 class Game():
-    def __init__(self):
-        self.grid = np.zeros((19,19))
-        self.mywindow = mywindow()
-        self.agent = solver()
+	def __init__(self):
+		self.grid = np.zeros((19,19))
+		self.mywindow = mywindow()
+		self.agent = solver()
--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from game import game
 
 if __name__ == "__main__":
     g = game()
+	initiate_instance(g)
+	for _ in range(10):
+	node = solver.find_best_move(node)
+	print('------------')
+	print(f'color: {node.color} | best score: {node.score()}')    
+	print(node.grid)
+	print('------------')
