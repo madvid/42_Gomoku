e86f6acf75146cf7b3fc75ce9756bd3e6264ec68
mdavid <mdavid@student.42.fr>
2021-12-14 20:08:57 +0100

    <Game Process>= wip : double free three detection, might be ok -> Test needded

--- constants.py
+++ constants.py
@@ -21,15 +21,4 @@ k_line = np.array([[BLACK, WHITE, WHITE, BLACK]])
 k_captures = {"line": k_line, "column": np.rot90(k_line), "diag1": k_diag, "diag2": np.rot90(k_diag)}
 
 # kernels to check if there is a free three
-k_freethree_l = np.array([[1, 2, 2, 2, 2, 1]])
-k_freethree_d = np.array([[1, 0, 0, 0, 0, 0],
-                          [0, 2, 0, 0, 0, 0],
-                          [0, 0, 2, 0, 0, 0],
-                          [0, 0, 0, 2, 0, 0],
-                          [0, 0, 0, 0, 2, 0],
-                          [0, 0, 0, 0, 0, 1]])
-
-k_free_threes = {"line": k_freethree_l,
-                 "column": np.rot90(k_freethree_l),
-                 "diag1": k_freethree_d,
-                 "diag2": np.rot90(k_freethree_d)}
+k_freethree = np.array([1, 2, 2, 2, 2, 1])
