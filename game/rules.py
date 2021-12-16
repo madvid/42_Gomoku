54f43f2699d975cefe98ffa6ed01896e9d519ea4
mdavid <mdavid@student.42.fr>
2021-12-16 19:22:20 +0100

    <Game Process>= refacto : capture position

--- game/rules.py
+++ game/rules.py
@@ -1,4 +1,6 @@
 import numpy as np
+from typing import List, Tuple
+from game.metrics import get_kern_row_idx, get_kern_col_idx, get_kern_diag_idx
 
 # =========================================================================== #
 #                          | constants definition |                           #
@@ -13,15 +15,13 @@ k_diag = np.array([[BLACK, 0,     0,     0],
                    [0,     0,     WHITE, 0],
                    [0,     0,     0,     BLACK]])
 
-k_line = np.array([[BLACK, WHITE, WHITE, BLACK]])
-
-k_captures = {"line": k_line, "column": np.rot90(k_line), "diag1": k_diag, "diag2": np.rot90(k_diag)}
+k_capture = np.array([BLACK, WHITE, WHITE, BLACK])
 
 # =========================================================================== #
 #                          | functions definition |                           #
 # =========================================================================== #
 
-def remove_opponent_pair(grid:np.array, last_coord:np.array, idx:int):
+def _DEPRECATED_remove_opponent_pair(grid:np.array, last_coord:np.array, idx:int):
     yx = last_coord
     #explicite coordinates of the stone to remove along each possible direction
     # from the stone just played
@@ -35,8 +35,12 @@ def remove_opponent_pair(grid:np.array, last_coord:np.array, idx:int):
                     (yx[0] + np.array([1, 2]), yx[1] + np.array([1, 2])))
     grid[stone_to_del[idx]] = 0
 
+def remove_opponent_pair(grid:np.array, coords_lst):
+    for pair in coords_lst:
+        grid[pair] = 0
 
-def iscapture_position(grid:np.array, last_coord:np.array, color:int) -> bool:
+
+def _DEPRECATED_iscapture_position(grid:np.array, last_coord:np.array, color:int) -> bool:
     """[summary]
     Args:
         yx ([type]): [description]
@@ -69,3 +73,37 @@ def iscapture_position(grid:np.array, last_coord:np.array, color:int) -> bool:
     if any([r == 4 for r in res]):
         return True
     return False
+
+
+def iscapture_position(grid:np.array, current_pos:np.array, color:int) -> List[Tuple[np.ndarray]]: # FIXME: create a understandable POSITION type alias 
+    """[summary]
+    Args:
+        yx ([type]): [description]
+        grid ([type]): [description]
+    Returns:
+        bool: [description]
+    """
+    yx = current_pos
+    pair_to_del = ((yx[0] + np.array([-2, -1]), yx[1] + np.array([0, 0])),
+                   (yx[0] + np.array([1, 2]), yx[1] + np.array([0, 0])),
+                   (yx[0] + np.array([0, 0]), yx[1] + np.array([-2, -1])),
+                   (yx[0] + np.array([0, 0]), yx[1] + np.array([1, 2])),
+                   (yx[0] + np.array([-2, -1]), yx[1] + np.array([-2, -1])),
+                   (yx[0] + np.array([-2, -1]), yx[1] + np.array([2, 1])),
+                   (yx[0] + np.array([2, 1]), yx[1] + np.array([-2, -1])),
+                   (yx[0] + np.array([1, 2]), yx[1] + np.array([1, 2])))
+    
+    # FIXME: use np.convolve instead
+    r_conv_c1 = np.sum(np.multiply(grid[get_kern_col_idx(yx)], color * k_capture))
+    r_conv_c2 = np.sum(np.multiply(grid[get_kern_col_idx(yx)], color * k_capture))
+    
+    r_conv_l1 = np.sum(np.multiply(grid[get_kern_row_idx(yx)], color * k_capture))
+    r_conv_l2 = np.sum(np.multiply(grid[get_kern_row_idx(yx)], color * k_capture))
+    
+    r_conv_d1 = np.sum(np.multiply(grid[get_kern_diag_idx(yx)], color * k_capture))
+    r_conv_d2 = np.sum(np.multiply(grid[get_kern_diag_idx(yx)], color * k_capture))
+    r_conv_d3 = np.sum(np.multiply(grid[get_kern_diag_idx(yx)], color * k_capture))
+    r_conv_d4 = np.sum(np.multiply(grid[get_kern_diag_idx(yx)], color * k_capture))
+    res = [r_conv_c1, r_conv_c2, r_conv_l1, r_conv_l2, r_conv_d1, r_conv_d2, r_conv_d3, r_conv_d4]
+    
+    return [pair_to_del[ii] for ii, r_conv in enumerate(res) if r_conv == 4]
